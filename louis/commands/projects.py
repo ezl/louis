d2ef2b9ea0821ede3826e47b9703fd57cb6c7819
Henrique Romano <chromano@gmail.com>
2011-07-12 18:47:32 -0300

    Install egenix-mx-base before project's requirements; it is most likely one of the requirements depends on mx-base than the opposite

--- louis/commands/projects.py
+++ louis/commands/projects.py
@@ -81,8 +81,8 @@ def install_project_requirements(project_username=project_username,
         extra_project_requirements()
     with settings(user=project_username):
         with cd('/home/%s' % project_username):
-            run('%s/bin/pip install -r %s' % (env_path, requirements_path))
             run('%s/bin/easy_install -i http://downloads.egenix.com/python/index/ucs4/ egenix-mx-base' % env_path)
+            run('%s/bin/pip install -r %s' % (env_path, requirements_path))
 
 def setup_project_code(project_name=project_name,
                        project_username=project_username,
